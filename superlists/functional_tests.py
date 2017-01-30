99b20633e07242c50496e9da8ab372223745f74a
Luke <novelview9@example.com>
2017-01-31 01:19:38 +0900

    change files

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -5,8 +5,8 @@ import unittest
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
-        self.browser = webdriver.Firefox()
-        self.browser.implicity_wait(3)
+        self.browser = webdriver.Chrome('chromedriver')
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
@@ -14,9 +14,27 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
 
         self.browser.get('http://localhost:8000')
-    
-        self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!') 
+
+        header_text = self.browser.find_element_by_css_selector("h1").text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element_by_css_selector("#id_new_item")
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                "input To-Do item"
+        )
+        
+        input.send_keys(Keys.Enter)
+        
+        table = self.browser.find_element_by_css_selector('#id_list_table')
+        rows = table.find_elements_by_css_selector('tr')
+        self.assertTrue(
+                any(row.text == '1: Buy the toy' for row in rows),
+        )
+        
+        self.fail('Finish the test!!')
+
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
