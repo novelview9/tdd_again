ed1a94fcc2bea0eb6a58293ac5e75c09e3b33f4f
Luke <novelview9@example.com>
2017-02-01 20:31:42 +0900

    add csrf token and solve the problem on testcase remove_csrf

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,3 +1,5 @@
+import re
+
 from django.template.loader import render_to_string
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
@@ -8,6 +10,11 @@ from lists.views import home_page
 
 class SmokeTest(TestCase):
 
+    @staticmethod
+    def remove_csrf(html_code):
+        csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>' 
+        return re.sub(csrf_regex, '', html_code)
+
     def test_root_url_resolvers_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
@@ -15,6 +22,16 @@ class SmokeTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
+        get_html = response.content.decode()
         self.assertIn(b'<title>To-Do lists</title>', response.content)
-        self.assertEqual(response.content.decode(), expected_html) 
+        expected_html = render_to_string('home.html')
+        self.assertEqual(self.remove_csrf(get_html), self.remove_csrf(expected_html))
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = "post"
+        request.POST["item_text"] = "new to-do item"
+        
+        response = home_page(request)
+
+        self.assertIn("new to-do item", response.content.decode())
